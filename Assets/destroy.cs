acab73b3ee643ace03873efae0e2677282be015a
belugasanity <aust.bagley@gmail.com>
2016-11-07 19:50:59 -0700

    Add files via upload

---
+++ Assets/destroy.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destroy : MonoBehaviour {
+	public float countDown;
+	public GameObject explosion;
+	
+	void OnCollisionEnter(Collision collision){
+		if(collision.gameObject.name == "Terrain"){
+			Instantiate(explosion,transform.position,transform.rotation);
+			Destroy (gameObject);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Destroy (gameObject, countDown);
+	}
+}
---
+++ Assets/fire.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fire : MonoBehaviour {
+
+public Rigidbody projectile;
+public Transform Spawnpoint;
+public int shootSpeed;
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Fire1")){//fire1 is left mouse button
+			Rigidbody clone;
+			clone = (Rigidbody)Instantiate(projectile, Spawnpoint.position, projectile.rotation);
+			clone.velocity = Spawnpoint.TransformDirection (Vector3.forward*shootSpeed);
+		}
+	}
+}
