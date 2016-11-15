e790369a8543c5567bcb74be7041e97610c32204
belugasanity <aust.bagley@gmail.com>
2016-11-14 21:41:44 -0700

    Coin and Counting score completion

--- Assets/coin.cs
+++ Assets/coin.cs
@@ -1,12 +1,8 @@
 ﻿using UnityEngine;
+using System.Collections;
 public class coin : MonoBehaviour
 {
-	void OnTriggerEnter(Collider other)
-	{
-		if (other.tag == "Player")
-		{
-			transform.GetComponent<Collider>().enabled = false;
-			transform.GetComponent<Renderer>().enabled = false;
-		}
+	void Update(){
+		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
 	}
 }
--- Assets/tankMovement.cs
+++ Assets/tankMovement.cs
@@ -1,14 +1,20 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class tankMovement : MonoBehaviour {
 
 	public float moveSpeed;
 	public float turnSpeed;
+	public Text countText;
+	private Rigidbody rb;
+	private int count;
 
 	// Use this for initialization
 	void Start () {
-	
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		SetCountText ();
 	}
 	
 	// Update is called once per frame
@@ -20,7 +26,19 @@ public class tankMovement : MonoBehaviour {
 		transform.Translate(0, 0, z);
 
 }
-	void OnTriggerEnter (Collider Coin){
-		Destroy(Coin.gameObject);
+	//void OnTriggerEnter (Collider Coin){
+	//	if (Coin.gameObject.CompareTag ("Pick Up")) {
+	//		Coin.gameObject.SetActive (false);
+	//	}
+	//}
+	void OnCollisionEnter(Collision other){
+		if(other.gameObject.CompareTag ("Pick Up")){
+			other.gameObject.SetActive(false);
+			count = count + 1;
+			SetCountText ();
+		}
+	}
+	void SetCountText (){
+		countText.text = "Count: " + count.ToString ();
 	}
 }
