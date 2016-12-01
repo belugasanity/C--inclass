67254d6e84e001faeb9e0ee7b512a35a3c22d381
belugasanity <aust.bagley@gmail.com>
2016-11-30 19:59:27 -0700

    make enemy, camera follows player

---
+++ Assets/follow.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class follow : MonoBehaviour {
+
+	public Rigidbody enemy;
+	public float moveSpeed;
+	public Transform target;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt (target);
+		transform.Translate (Vector3.forward * moveSpeed * Time.deltaTime);
+	}
+}
